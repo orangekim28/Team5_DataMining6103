cd40ca6ebdc96343ab83603d1d7a4078fbe20bb4
duvvi14 <bharath1496@gwmail.gwu.edu>
2022-12-08 17:49:37 -0500

    Project File

--- Project.py
+++ Project.py
@@ -1,17 +1,1 @@
-#%%
-
-import pandas as pd
-
-#%%
-
-df = pd.read_csv("creditcard.csv")
-# %%
-
-df.info()
-# %%
-
-df.head()
-# %%
-
-df.tail()
 # %%
---
+++ Project_File.py
@@ -0,0 +1,17 @@
+#%%
+
+import pandas as pd
+
+#%%
+
+df = pd.read_csv("creditcard.csv")
+# %%
+
+df.info()
+# %%
+
+df.head()
+# %%
+
+df.tail()
+# %%
