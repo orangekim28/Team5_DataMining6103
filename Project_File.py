0e00f334a07786c4bc33d467428a0fba8c930347
avelinemariya97 <avelinemariya@gmail.com>
2022-12-09 10:20:17 -0500

    Update Project_File.py

--- Project_File.py
+++ Project_File.py
@@ -13,7 +13,7 @@ import pandas as pd
 #%%
 # Load the Credit Card Dataset into a DataFrame (CCFD_DATA)
 
-CCFD_DATA=pd.read_csv('creditcard.csv')
+CCFD_DATA =pd.read_csv('creditcard.csv')
 CCFD_DATA.shape
 
 #%%
@@ -70,5 +70,46 @@ print("Percentage of Fraud transactions: {:.4f}".format(fraud_percent))
 # Summary Statistics
 
 CCFD_DATA.describe()
+
+# %%
+# Exploratory Data Analysis
+# Importing Libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pylab import rcParams
+
+# %%
+CCFD_DATA.hist(figsize=(20,20),color='lime')
+plt.show()
+
+# %%
+
+CCFD_DATA['Class'].value_counts().plot(kind='bar')
+plt.xticks([0,1],['Genuine', 'Fraud'])
+
+
+# %%
+## Since the data is highly imbalanced, we will move ahead with Undersampling/Upsampling.
+### But inorder to see which method works for the data, let's create model using upsampled and undersampled data.
+## We need balanced data!!
+# class count
+class_count_0, class_count_1 = CCFD_DATA['Class'].value_counts()
+
+# Separate class
+class_0 = CCFD_DATA[CCFD_DATA['Class'] == 0]
+class_1 = CCFD_DATA[CCFD_DATA['Class'] == 1]
+# print the shape of the class
+print('class 0:', class_0.shape)
+print('class 1:', class_1.shape)
+      
+# %%
+# Undersampling Technique
+class_0_under = class_0.sample(class_count_1)
+
+test_under = pd.concat([class_0_under, class_1], axis=0)
+
+print("total class of 1 and0:",test_under['Class'].value_counts())
+# plot the count after under-sampeling
+test_under['Class'].value_counts().plot(kind='bar', title='count (target)')
 
 # %%
