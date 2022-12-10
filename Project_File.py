c1f8c595ccd7d90cf4dbc837f603c4ddf9af6c96
duvvi14 <bharath1496@gwmail.gwu.edu>
2022-12-10 14:41:02 -0500

    Alignment Changes

--- Project_File.py
+++ Project_File.py
@@ -73,12 +73,14 @@ CCFD_DATA.describe()
 
 # %%
 # Exploratory Data Analysis
+
 # Importing Libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pylab import rcParams
 
 # %%
+
 CCFD_DATA.hist(figsize=(20,20),color='lime')
 plt.show()
 
@@ -87,7 +89,7 @@ plt.show()
 CCFD_DATA['Class'].value_counts().plot(kind='bar')
 plt.xticks([0,1],['Genuine', 'Fraud'])
 
-
+#
 # %%
 ## Since the data is highly imbalanced, we will move ahead with Undersampling/Upsampling.
 ### But inorder to see which method works for the data, let's create model using upsampled and undersampled data.
@@ -102,31 +104,23 @@ class_1 = CCFD_DATA[CCFD_DATA['Class'] == 1]
 print('class 0:', class_0.shape)
 print('class 1:', class_1.shape)
       
-      
-      
-      
-      
-      
-      
+#   
 # %%
 # Undersampling Technique
 class_0_under = class_0.sample(class_count_1)
 data_under = pd.concat([class_0_under, class_1], axis=0)
-
 print("total class of 1 and0:",data_under['Class'].value_counts())
+
 # plot the count after under-sampeling
 data_under['Class'].value_counts().plot(kind='bar', title='count (target)')
 
 # %%
-
-## correlation matrix.
-
+# Correlation matrix
 plt.figure(figsize=(10,8))
 corr=data_under.corr()
 sns.heatmap(corr,cmap='BuPu')
 
 # %%
-
 # Splitting the Data
 from sklearn.model_selection import train_test_split
 
@@ -135,9 +129,9 @@ y=data_under['Class']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=123)
 
 # %%
-
-## Model 1
+# Model 1
 ## Random Forest Classifier
+
 from sklearn.ensemble import RandomForestClassifier
 
 # Fit and predict
@@ -153,9 +147,10 @@ print("The precision is", precision_score(y_test, y_pred))
 print("The recall is", recall_score(y_test, y_pred))
 print("The F1 score is", f1_score(y_test, y_pred))
 
-# classification report
+# Classification report
 print(classification_report(y_test, y_pred))
-# confusion matrix
+
+# Confusion matrix
 fig, ax = plt. subplots ()
 sns.heatmap (confusion_matrix(y_test, y_pred, normalize='true'), annot=True, ax=ax)
 ax.set_title ("Confusion Matrix")
@@ -163,8 +158,7 @@ ax.set_ylabel("Real Value")
 ax.set_xlabel("Predicted")
 
 # %%
-
-## Model 2
+# Model 2
 ## Logistic Regression
 
 from sklearn.linear_model import LogisticRegression
@@ -179,9 +173,10 @@ print("The precision is", precision_score(y_test, y_pred_2))
 print("The recall is", recall_score(y_test, y_pred_2))
 print("The F1 score is", f1_score(y_test, y_pred_2))
 
-# classification report
+# Classification report
 print(classification_report(y_test, y_pred_2))
-# confusion matrix
+
+# Confusion matrix
 fig, ax = plt. subplots ()
 sns.heatmap (confusion_matrix(y_test, y_pred_2, normalize='true'), annot=True, ax=ax)
 ax.set_title ("Confusion Matrix")
@@ -189,8 +184,7 @@ ax.set_ylabel("Real Value")
 ax.set_xlabel("Predicted")
 
 # %%
-
-## Model 3
+# Model 3
 ## Decision Tree
 
 from sklearn.tree import DecisionTreeRegressor
@@ -204,9 +198,10 @@ print("The precision is", precision_score(y_test, y_pred_3))
 print("The recall is", recall_score(y_test, y_pred_3))
 print("The F1 score is", f1_score(y_test, y_pred_3))
 
-# classification report
+# Classification report
 print(classification_report(y_test, y_pred_3))
-# confusion matrix
+
+# Confusion matrix
 fig, ax = plt. subplots ()
 sns.heatmap (confusion_matrix(y_test, y_pred_3, normalize='true'), annot=True, ax=ax)
 ax.set_title ("Confusion Matrix")
@@ -214,8 +209,7 @@ ax.set_ylabel("Real Value")
 ax.set_xlabel("Predicted")
 
 # %%
-
-## KNN Model
+# KNN Model
 
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -259,14 +253,14 @@ knn = KNeighborsClassifier(n_neighbors = 4)
 knn.fit(X_train,y_train)
 y_pred_4 = knn.predict(X_test)
 
-# classification report
+# Classification report
 print(classification_report(y_test, y_pred_4))
-# confusion matrix
+
+# Confusion matrix
 fig, ax = plt. subplots ()
 sns.heatmap (confusion_matrix(y_test, y_pred_4, normalize='true'), annot=True, ax=ax)
 ax.set_title ("Confusion Matrix")
 ax.set_ylabel("Real Value")
 ax.set_xlabel("Predicted")
 
-
-# %%
+#
