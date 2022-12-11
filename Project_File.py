2bebe4883577ceef259822cdb8b6e47d3ee72eec
duvvi14 <bharath1496@gwmail.gwu.edu>
2022-12-11 13:24:16 -0500

    Modelling

--- Project_File.py
+++ Project_File.py
@@ -183,6 +183,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=1
 ## Random Forest Classifier
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix 
 
 # Fit and predict
 rfc = RandomForestClassifier() 
@@ -212,7 +213,6 @@ ax.set_xlabel("Predicted")
 ## Logistic Regression
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix 
 
 lr=LogisticRegression()
 lr.fit(X_train,y_train)
@@ -319,4 +319,154 @@ ax.set_title ("Confusion Matrix")
 ax.set_ylabel("Real Value")
 ax.set_xlabel("Predicted")
 
-#
+
+# %%
+# Splitting the Data : Over Sampling
+from sklearn.model_selection import train_test_split
+
+X=data_over.drop(['Class'],axis=1)
+y=data_over['Class']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=123)
+
+# %%
+# Model 1
+## Random Forest Classifier : Over Sampling
+
+from sklearn.ensemble import RandomForestClassifier
+
+# Fit and predict
+rfc = RandomForestClassifier() 
+rfc.fit(X_train, y_train) 
+y_pred = rfc.predict(X_test)
+
+# For the performance let's use some metrics from SKLEARN module
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+  
+print("The accuracy is", accuracy_score(y_test, y_pred)) 
+print("The precision is", precision_score(y_test, y_pred))
+print("The recall is", recall_score(y_test, y_pred))
+print("The F1 score is", f1_score(y_test, y_pred))
+
+# Classification report
+print(classification_report(y_test, y_pred))
+
+# Confusion matrix
+fig, ax = plt. subplots ()
+sns.heatmap (confusion_matrix(y_test, y_pred, normalize='true'), annot=True, ax=ax)
+ax.set_title ("Confusion Matrix")
+ax.set_ylabel("Real Value")
+ax.set_xlabel("Predicted")
+
+# %%
+# Model 2
+## Logistic Regression : Over Sampling
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix 
+
+lr=LogisticRegression()
+lr.fit(X_train,y_train)
+y_pred_2=lr.predict(X_test)
+
+print("The accuracy is", accuracy_score(y_test, y_pred_2)) 
+print("The precision is", precision_score(y_test, y_pred_2))
+print("The recall is", recall_score(y_test, y_pred_2))
+print("The F1 score is", f1_score(y_test, y_pred_2))
+
+# Classification report
+print(classification_report(y_test, y_pred_2))
+
+# Confusion matrix
+fig, ax = plt. subplots ()
+sns.heatmap (confusion_matrix(y_test, y_pred_2, normalize='true'), annot=True, ax=ax)
+ax.set_title ("Confusion Matrix")
+ax.set_ylabel("Real Value")
+ax.set_xlabel("Predicted")
+
+# %%
+# Model 3
+## Decision Tree : Over Sampling
+
+from sklearn.tree import DecisionTreeRegressor
+
+dt=DecisionTreeRegressor()
+dt.fit(X_train,y_train)
+y_pred_3=dt.predict(X_test)
+
+print("The accuracy is", accuracy_score(y_test, y_pred_3)) 
+print("The precision is", precision_score(y_test, y_pred_3))
+print("The recall is", recall_score(y_test, y_pred_3))
+print("The F1 score is", f1_score(y_test, y_pred_3))
+
+# Classification report
+print(classification_report(y_test, y_pred_3))
+
+# Confusion matrix
+fig, ax = plt. subplots ()
+sns.heatmap (confusion_matrix(y_test, y_pred_3, normalize='true'), annot=True, ax=ax)
+ax.set_title ("Confusion Matrix")
+ax.set_ylabel("Real Value")
+ax.set_xlabel("Predicted")
+
+# %%
+# Model 4
+# KNN Model : Over Sampling
+
+from sklearn.neighbors import KNeighborsClassifier
+
+neighbours = np.arange(1,25)
+train_accuracy =np.empty(len(neighbours))
+test_accuracy = np.empty(len(neighbours))
+
+for i,k in enumerate(neighbours):
+    #Setup a knn classifier with k neighbors
+    knn=KNeighborsClassifier(n_neighbors=k,algorithm="kd_tree",n_jobs=-1)
+    
+    #Fit the model
+    knn.fit(X_train,y_train.ravel())
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train.ravel())
+    
+    #Compute accuracy on the test set
+    test_accuracy[i] = knn.score(X_test, y_test.ravel()) 
+
+#Generate plot
+plt.title('k-NN Varying number of neighbors')
+plt.plot(neighbours, test_accuracy, label='Testing Accuracy')
+plt.plot(neighbours, train_accuracy, label='Training accuracy')
+plt.legend()
+plt.xlabel('Number of neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+
+idx = np.where(test_accuracy == max(test_accuracy))
+x = neighbours[idx]
+
+#k_nearest_neighbours_classification
+knn=KNeighborsClassifier(n_neighbors=x[0],algorithm="kd_tree",n_jobs=-1)
+knn.fit(X_train,y_train.ravel())
+
+# %%
+
+knn = KNeighborsClassifier(n_neighbors = 4) 
+knn.fit(X_train,y_train)
+y_pred_4 = knn.predict(X_test)
+
+print("The accuracy is", accuracy_score(y_test, y_pred_4)) 
+print("The precision is", precision_score(y_test, y_pred_4))
+print("The recall is", recall_score(y_test, y_pred_4))
+print("The F1 score is", f1_score(y_test, y_pred_4))
+
+# Classification report
+print(classification_report(y_test, y_pred_4))
+
+# Confusion matrix
+fig, ax = plt. subplots ()
+sns.heatmap (confusion_matrix(y_test, y_pred_4, normalize='true'), annot=True, ax=ax)
+ax.set_title ("Confusion Matrix")
+ax.set_ylabel("Real Value")
+ax.set_xlabel("Predicted")
+
+# %%
